3f39240107fcc8b1cfef95e6f63c0dc8dd3c81ad
zzaakiirr <zzaakiirr@gmail.com>
2018-04-06 00:09:41 +0300

    Fixed run.py with name == main

--- run.py
+++ run.py
@@ -2,10 +2,11 @@ from bs4 import BeautifulSoup
 
 import request_helpers
 
-entered_question = input()
-response = request_helpers.get_response(entered_question)
+if __name__ == '__main__':
+    entered_question = input()
+    response = request_helpers.get_response(entered_question)
 
-soup = BeautifulSoup(response, 'html.parser')
-links = soup.find_all('div', 'result-link')
-for link in links:
-    print('https://stackoverflow.com%s' % link.a['href'])
+    soup = BeautifulSoup(response, 'html.parser')
+    links = soup.find_all('div', 'result-link')
+    for link in links:
+        print('https://stackoverflow.com%s' % link.a['href'])
